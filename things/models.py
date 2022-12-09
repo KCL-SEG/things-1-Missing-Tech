8f78b9f4b5440f217a1016a656ebb52da1eb5417
Missing-Tech <josephgrabski31@gmail.com>
2022-12-09 13:03:42 +0000

    Add validators to model

--- things/models.py
+++ things/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Thing(models.Model):
-    name = models.CharField(max_length=30)
-    description = models.TextField(max_length=200)
-    quantity = models.IntegerField()
+    name = models.CharField(max_length=30, blank=False, unique=True)
+    description = models.TextField(max_length=120, blank=True, unique=False)
+    quantity = models.IntegerField(unique=False, min_value=0, max_value=100)
