9d61f7a707d17fd88231911967792b0f3df30873
Missing-Tech <josephgrabski31@gmail.com>
2022-12-09 13:06:44 +0000

    Fix validators

--- things/models.py
+++ things/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 class Thing(models.Model):
     name = models.CharField(max_length=30, blank=False, unique=True)
     description = models.TextField(max_length=120, blank=True, unique=False)
-    quantity = models.IntegerField(unique=False, min_value=0, max_value=100)
+    quantity = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)], unique=False)
